dd0bfbc8c1437b933a8f9c07dc7bfefbaf0c85d3
Denzel <odenzel949@gmail.com>
2025-05-12 18:28:59 -0300

    Implementação do ORM na camada de domain

---
+++ src/main/java/com/bootcamp/dio/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package com.bootcamp.dio.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_feature")
+public class Feature extends BaseItem{
+}
---
+++ src/main/java/com/bootcamp/dio/domain/model/News.java
@@ -0,0 +1,9 @@
+package com.bootcamp.dio.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_news")
+public class News extends BaseItem{
+}
